e2bd25e501f3bac5f4aaefe5583e22175af647c2
CarlosColnaghi <carlos.colnaghi@hotmail.com>
2021-11-21 20:14:57 -0300

    implementing the function that returns random numbers

--- openmp.c
+++ openmp.c
@@ -1,21 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 #include<omp.h>
 
-void Hello(void);
+float* generate_random_numbers(float a, float b, int n, int thread_count){
+    float* array = malloc(sizeof(float) * n);
+    srand(time(0));
+#   pragma omp parallel for num_threads(thread_count)
+    for(int i = 0; i < n; i++){
+        float scale = rand() / (float)RAND_MAX;
+        array[i] = a + scale * (b-a);
+    }
+    return array;
+}
 
 int main(int argc, char* argv[]){
     int thread_count = strtol(argv[1], NULL, 10);
 
-#   pragma omp parallel num_threads(thread_count)
-    Hello();
+    generate_random_numbers(0, 10, 10, thread_count);
 
     return 0;
-}
-
-void Hello(void){
-    int my_rank = omp_get_thread_num();
-    int thread_count = omp_get_num_threads();
-
-    printf("Hello da thread %d de %d\n", my_rank, thread_count);
 }
